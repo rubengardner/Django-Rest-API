444bc78a2e1ad4732e2580524b1724899fa18ce4
rubengardner <rgardnercuesta@gmail.com>
2023-04-17 15:19:32 +0000

    Add image validation

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,3 +1,4 @@
+
 from rest_framework import serializers
 from .models import Profile
 
@@ -6,6 +7,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
