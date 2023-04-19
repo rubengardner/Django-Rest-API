81a684ccca1a851ab00575ee0160793f1d00f5ae
rubengardner <rgardnercuesta@gmail.com>
2023-04-19 14:55:42 +0000

    Add followers urls

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('comments.urls')),
+    path('', include('likes.urls')),
+    path('', include('followers.urls')),
     path('api_auth/', include('rest_framework.urls'))
 ]
